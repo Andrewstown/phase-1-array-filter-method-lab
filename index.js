cb9703975fb6fc4a086571f934d2722f21b3f8e4
+ <stingeyandrewstown@live.com>
2023-01-11 08:56:08 -0600

    commit tree 6024979152f6d036647faedbe543790f5dba7968

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, guy){
+    return drivers.filter(index => index.toLowerCase() == guy.toLowerCase());
+}
+
+function fuzzyMatch(drivers, guy){
+    return drivers.filter(index => index.substring(0, guy.length) == guy)
+}
+
+function matchName(drivers, guy){
+    return drivers.filter(index => index.name == guy)
+}
